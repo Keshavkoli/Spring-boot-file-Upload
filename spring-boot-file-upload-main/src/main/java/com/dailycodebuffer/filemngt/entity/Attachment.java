6bdc539805105db6808a259364fd37d676c3a40d
Keshav <keshavkoli191@gmail.com>
2023-03-28 17:33:35 +0530

    Spring-boot-File-Upload

---
+++ spring-boot-file-upload-main/src/main/java/com/dailycodebuffer/filemngt/entity/Attachment.java
@@ -0,0 +1,33 @@
+package com.dailycodebuffer.filemngt.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Attachment {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String fileName;
+    private String fileType;
+
+    @Lob // what ever the file you are going to upload that will be stored as a bite array data in the Lob
+    private byte[] data;
+
+    public Attachment(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+}
